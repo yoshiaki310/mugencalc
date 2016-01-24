33d1e99934aa6bdb99a370376f07c600fd7a6dec
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2016-01-25 01:26:17 +0900

    karmaのテストをPhantomJSで走らせるように切り替え

--- karma.conf.js
+++ karma.conf.js
@@ -15,13 +15,12 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+            'karma-phantomjs-launcher'
             ],
 
     junitReporter : {
