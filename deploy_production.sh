d3f2e2f1adf8ce709f7fe7fe8a219e4ec9e88cb4
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2016-01-26 01:25:47 +0900

    S3のリージョン指定追加

--- deploy_production.sh
+++ deploy_production.sh
@@ -2,5 +2,6 @@
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=mugencalc.satoh.xyz
+REGION=ap-northeast-1
 DIR=./app/
-aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE" --region="$REGION"
