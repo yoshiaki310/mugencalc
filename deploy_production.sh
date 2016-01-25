51401673f3710f00ee5dd20bc23b4aa7b930d0a0
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2016-01-26 01:20:37 +0900

    バケット名変更

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=mugencalc
+BUCKET=mugencalc.satoh.xyz
 DIR=./app/
 aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
