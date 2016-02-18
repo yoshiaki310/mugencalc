e78f914ba575d40551e0de5bde05e0aa173cf8c3
yoshiaki310 <yoshiaki.satoh@gmail.com>
2016-02-19 00:48:47 +0900

    github pagesで充分そうなのでs3へのデプロイを削除し、circle.ymlに直接書く

--- deploy_production.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DEFAULT="default"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=mugencalc.satoh.xyz
-REGION=ap-northeast-1
-DIR=./app/
-aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE" --region="$REGION"
-
-git subtree push --prefix app origin gh-pages
